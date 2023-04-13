02c660af3550a2281505489fc0b08066971b169d
amarnath <vanreddy999@gmail.com>
2023-04-13 12:43:12 +0530

    this is commit from Asian timezone guy

---
+++ src/test/java/testapplication/Vowels.java
@@ -0,0 +1,25 @@
+package testapplication;
+
+public class Vowels {
+
+	public static void main(String[] args) {
+
+		
+		String str = "Amarnath";
+		str=str.toLowerCase();
+		int count= 0;
+
+		for (int i = 0; i < str.length(); i++){
+
+		if (str.charAt(i) =='a' || str.charAt(i) =='e'||str.charAt(i)=='i'|| str.charAt(i)=='o'||str.charAt(i)=='u')
+		{
+		count++;
+		}
+		}
+
+		System.out.println("total no. of vowels in string are:" +count);
+		}	
+	
+	}
+
+
